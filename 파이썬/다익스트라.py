79767ade8636bce8c5b5e1ba900090f6688a96b0
Joonw00 <97094633+Joonw00@users.noreply.github.com>
2022-11-15 23:17:25 +0900

    Add files via upload

---
+++ 파이썬/다익스트라.py
@@ -0,0 +1,35 @@
+import heapq
+import sys
+n = int(sys.stdin.readline())
+m = int(sys.stdin.readline())
+graph = [[] for _ in range(n + 1)]
+for i in range(m):
+    a,b,w = map(int, sys.stdin.readline().split())
+    graph[a].append((b,w))
+
+start, end = map(int, sys.stdin.readline().split())
+
+INF = int(1e9)
+distance = [INF] * (n + 1)
+
+
+def dijkstra(start):
+    q = []
+    # 시작 노드로 가기 위한 최단 경로는 0으로 설정하여, 큐에 삽입
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    # 큐가 비어있지 않다면
+    while q:
+        # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
+        dist, now = heapq.heappop(q)
+        # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
+        if distance[now] < dist:
+            continue
+        # 현재 노드와 연결된 다른 인접한 노드들을 확인
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+dijkstra(start)
+print(distance[end])
